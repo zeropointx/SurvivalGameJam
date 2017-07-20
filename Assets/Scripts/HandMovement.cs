264491b452c0758dff9ac44e9e7a8507a226d86e
Zegram <Joikkiz@hotmail.com>
2017-07-20 15:44:06 +0300

    Unity project + git settings.

---
+++ Assets/Scripts/HandMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour {
+
+    public GameObject leftHand = null;
+    public GameObject rightHand = null;
+    public GameObject playerModel = null;
+
+    private Vector3 clickStartCoordinate;
+    private Vector3 clickEndCoordinate;
+
+
+    private bool mouseDown = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDown = true;
+            clickStartCoordinate = GetMousePosition();
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+        }
+
+        if (mouseDown)
+            FistMovement(clickStartCoordinate);
+       
+	}
+
+    void FistMovement(Vector3 clickStart)
+    {
+
+    }
+
+    Vector3 GetMousePosition()
+    {
+        return Input.mousePosition;
+        
+    }
+}
