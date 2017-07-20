26d69816c929d1cbdea2cb451794f5281c6029c5
zeropointx <email@peruna.host>
2017-07-20 16:00:17 +0300

    Added new map, added playerstart script to modify player changes without saving them to map.

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSpawn : MonoBehaviour {
+    public GameObject playerPrefab = null;
+	void Start ()
+    {
+        GameObject.Instantiate(playerPrefab, transform);
+    }
+    void Update ()
+    {
+		
+	}
+}
