1a2d53a433b54dc620c54e8ae241f6cfef86a82b
zeropointx <email@peruna.host>
2017-07-26 21:37:26 +0300

    Added arty stuff. Added basic AI that follows player

---
+++ Assets/AI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+    NavMeshAgent navMeshAgent = null;
+	// Use this for initialization
+	void Start ()
+    {
+        navMeshAgent = transform.GetComponent<NavMeshAgent>();
+	}
+	Vector3 getTarget()
+    {
+       var player =  GameObject.FindGameObjectWithTag("Player");
+        return player.transform.position;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        navMeshAgent.SetDestination(getTarget());
+	}
+}
